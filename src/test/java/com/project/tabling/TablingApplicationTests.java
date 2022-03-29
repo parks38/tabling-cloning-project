35b54d2627072c9ba63f85d92fd1e0b9de8f33a4
sunny <parks38@uw.edu>
2022-03-29 16:42:39 +0900

    feat: initial project setting

---
+++ src/test/java/com/project/tabling/TablingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tabling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
